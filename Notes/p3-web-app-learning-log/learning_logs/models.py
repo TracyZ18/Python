ff13720d28ec4ccf372f220138d15a90ba001f0b
Tracy <46543122+TracyZ18@users.noreply.github.com>
2020-02-21 21:48:46 -0500

    created model "Topic"

--- Notes/p3-web-app-learning-log/learning_logs/models.py
+++ Notes/p3-web-app-learning-log/learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model) :
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=200) 
+    # CharField, stores small amout of text
+    date_added = models.DateTimeField(auto_now_add=True) 
+    # auto_now_add, automatically add current date when create a new topic
+
+    def __str__(self): # similar to java's toString()
+        """Return a string representation of the model."""
+        return self.text
