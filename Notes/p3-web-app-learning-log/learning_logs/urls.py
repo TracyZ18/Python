984381134485a5a9472c30b95164c6306a2ccf44
Tracy <46543122+TracyZ18@users.noreply.github.com>
2020-02-22 21:42:22 -0500

    urls, views, templates

--- Notes/p3-web-app-learning-log/learning_logs/urls.py
+++ Notes/p3-web-app-learning-log/learning_logs/urls.py
@@ -1,7 +1,19 @@
 """Defines URL patterns for learning_logs."""
 from django.conf.urls import url
 from . import views
+
+app_name = 'learning_logs'
+# must specify app_name to use include()
+
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+    # for each name
+    # need to define function in views.py
+
+    # Show all topics.
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
