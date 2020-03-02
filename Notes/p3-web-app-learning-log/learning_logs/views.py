0a6d35015af5c1af595d24cb2c7953d33462807d
Tracy <46543122+TracyZ18@users.noreply.github.com>
2020-03-01 20:39:04 -0500

    user new topic and entries

--- Notes/p3-web-app-learning-log/learning_logs/views.py
+++ Notes/p3-web-app-learning-log/learning_logs/views.py
@@ -21,3 +21,42 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
     # topic.html will have access to variables
     # 'topic' and 'entries' since they are keys of 'context'
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import TopicForm,EntryForm
+
+def new_topic(request): 
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('learning_logs:topics'))
+    
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    # an entry must be associated with a topic (determined by topic_id)
+    topic = Topic.objects.get(id=topic_id) 
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+    # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+    if form.is_valid():
+        new_entry = form.save(commit=False)
+        new_entry.topic = topic
+        new_entry.save()
+        return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
