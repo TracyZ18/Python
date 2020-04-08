bf773402b262207b49a6c1c8ae608218c0d56e40
Tracy <46543122+TracyZ18@users.noreply.github.com>
2020-04-08 16:18:33 -0400

    remove django

--- Notes/p3-web-app-learning-log/learning_logs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# import model(s) from an app,
-# then tell Django to manage model through admin site
-from learning_logs.models import Topic, Entry
-admin.site.register(Topic) 
-admin.site.register(Entry)
